25a06b3785e1f540596f5a2934dbe0fb5582c268
Miguel <9401997@alumnos.ufv.es>
2025-04-07 19:20:35 +0200

    WIP: Save local changes before rebase

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d80d59edd76feda34ccf1b4e8c757bab55d416a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39658704aaaef35a6b89d54aaa98da497f3a51c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
