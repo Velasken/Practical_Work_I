05035d3059d0b0739dd0245e465b13a3b929f127
Miguel <9401997@alumnos.ufv.es>
2025-04-09 18:48:26 +0200

    Getters

--- ConsoleApp1/Aircraft.cs
+++ ConsoleApp1/Aircraft.cs
@@ -4,10 +4,44 @@ namespace PracticalWotkI
 {
     public class Aircraft
     {
-        private string id;
-        private int distance;
-        private double fuel_capacity;
-        private double fuel_consumption;
-        private double current_fuel;
+        protected string id;
+        protected int distance;
+        protected double fuel_capacity;
+        protected double fuel_consumption;
+        protected double current_fuel;
+
+        public Aircraft(string id, int distance, double fuel_capacity, double fuel_consumption, double current_fuel)
+        {
+            this.id = id;
+            this.distance = distance;
+            this.fuel_capacity = fuel_capacity;
+            this.fuel_consumption = fuel_consumption;
+            this.current_fuel = current_fuel;
+        }
+
+        public string GetID()
+        {
+            return this.id;
+        }
+
+        public int GetDistance()
+        {
+            return this.distance;
+        }
+
+        public double GetFUelCapacity()
+        {
+            return this.fuel_capacity;
+        }
+
+         public double GetFUelConsumption()
+        {
+            return this.fuel_consumption;
+        }
+
+         public double GetCurrentFuel()
+        {
+            return this.current_fuel;
+        }
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58f0190b1df7d72af34baee7c8c85228aed9c0ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc881902b1fc76b02ac91cc3cfc0bade7fba08a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
