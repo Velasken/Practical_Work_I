c9d8528bc2ead499bb1ea59e06088c0570e35801
alfonloz <alflozher1@gmail.com>
2025-04-17 12:34:21 +0200

    probando haber si me daja editar de una vez el repositorio.

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49015310144fb6ae64da6fbd740548a0c5dd01e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f4db4a71e3a50aaa24184593f21a4e14fbd61a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
