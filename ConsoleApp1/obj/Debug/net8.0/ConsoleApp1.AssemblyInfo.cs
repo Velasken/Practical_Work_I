28477904d9d6f3b520e1b0c4ab6f6d4ac195f399
alfonloz <alflozher1@gmail.com>
2025-04-17 17:55:16 +0200

    añadiendo RemoveLandedAircraft

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4633df53ce2da8291429423a3e1431e32ad9592d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc00910fd712325cbacde329726107b0062f609a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
